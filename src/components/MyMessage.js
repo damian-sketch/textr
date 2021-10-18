d64b5ebc75ace3cbe90218f60090586a902a286e
damian-sketch <cedricdamian19@gmail.com>
2021-10-18 17:08:35 +0300

    Send messages and images

---
+++ src/components/MyMessage.js
@@ -0,0 +1,21 @@
+const MyMessage = ( {message} ) => {
+    if(message.attachments && message.attachments.length > 0) {
+        return (
+            <img 
+               src={message.attachments[0].file}
+               alt="message-attachment"
+               className="message-image"
+               style={{ float: 'right' }}
+            />
+        )
+    }
+    return (
+        
+        <div className="message" style={{ float: 'right', color: 'white', marginRight: '18px', backgroundColor: 'blue' }}>
+            {message.text}
+        </div>
+    )
+    
+}
+
+export default MyMessage;
---
+++ src/components/TheirMessage.js
@@ -0,0 +1,34 @@
+const TheirMessage = ( lastMessage, message) => {
+    const isFirstMessageByUser = !lastMessage || lastMessage.sender.username !== message.sender.username;
+
+    return (
+        // if it's the first message
+        <div className="message-row">
+            {isFirstMessageByUser && (
+                <div 
+                  className="message-avatar"
+                  style={{backgroundImage: `url(${message?.sender?.avatar})`}}
+                />
+            )}
+
+                 {message?.attachments?.length > 0 // if it's an image
+                    ? (
+                        <img 
+                           src={message.attachments[0].file}
+                           alt="Message attachment"
+                           className="message-image"
+                           style={{ marginLeft: isFirstMessageByUser ? '4px' : '48px' }}
+                        />
+                    ) : (
+                        <div className="message" style={{ float: 'left', backgroundColor: 'green', marginLeft: isFirstMessageByUser ? '4px' : '48px'  }}>
+                            {message.text}
+                         </div>
+                    )
+                }
+
+            TheirMessage
+        </div>
+    )
+}
+
+export default TheirMessage;
